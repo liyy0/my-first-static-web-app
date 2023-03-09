43700e63357836b818c4e5bed10d61e211a9740c
Liyy <liyy@crc-dot1x-nat-10-239-93-109.bu.edu>
2023-03-09 04:07:35 -0500

    almost finished

---
+++ src/App.tsx
@@ -0,0 +1,116 @@
+import React, { useEffect, useState } from "react";
+import Grid from "@mui/material/Unstable_Grid2";
+import { Typography } from "@mui/material";
+import { DataGrid, GridColDef, GridValueGetterParams } from '@mui/x-data-grid';
+import { BASE_URL,GET_DEFAULT_HEADERS } from "./globals";
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import express from 'express'
+import Button from '@mui/material/Button';
+import * as dotenv from 'dotenv'
+// // const path = require('path-browserify')
+// dotenv.config();
+
+// let api_key = process.env.api_key_keyvault || ""
+let api_key = "jWEgIXzfodOaGQKxty7JR3hxGooA1HhT1f3Py7KeMAClAzFuUx63tA==";
+function App() {
+  // // You will need to use more of these!
+  const [curShipperId, setcurShipperId] = useState<string>("");
+  const [curShipment, setcurShipment] = useState([]);
+  const [errorMsg, setErrorMsg] = useState<string>("");
+  const [pagesize,setPageSize] = useState<number>(5);
+
+  const fetchSomeData = async () => {
+    const res = await fetch(`${BASE_URL}ShipperId=${curShipperId}&code=${api_key}`, {
+          method: 'get',
+          headers:GET_DEFAULT_HEADERS(),
+          credentials: "include"
+        });
+    console.log(api_key);
+    const json = await res.json();
+    console.log(json)
+    return json
+    
+  };
+  // useEffect(() => {
+
+  // })
+
+  
+  const submitHandler = (event:any) => {
+    event.preventDefault();
+    console.log(curShipperId);
+    // debugger;
+
+    fetchSomeData()
+    .then(
+      res =>{
+        if(res.length===0){
+          setcurShipment([]);
+          setErrorMsg("Invalid Shipper Id. Please check");
+        }else{
+          setcurShipment(res);
+          setErrorMsg("");
+        }
+      }
+    )
+  }
+
+  const columns: GridColDef[] = [
+    { field: 'Date', headerName: 'Date', width: 130 },
+    { field: 'WarehouseID', headerName: 'Warehouse ID', width: 300 },
+    { field: 'ShippingPO', headerName: 'CShipping PO', width: 300 },
+    { field: 'ShipmentID', headerName: 'Shipment ID', width: 100 },
+    { field: 'BoxesRcvd', headerName: 'Boxes Received', width: 100 },
+  ];
+  
+  const rows = [
+    { id: 1, StudentName: 'abc', ClassId: 'C123', ClassName: 'ABCD', Semester: '123'},
+
+  ];
+
+  return (
+    <div style={{ width: "100vw", height: "100vh" }}>
+      <Grid container spacing={2} style={{ padding: "1rem" }}>
+        <Grid xs={12} container alignItems="center" justifyContent="center">
+          <Typography variant="h2" gutterBottom>
+            Report
+          </Typography>
+        </Grid>
+        <Grid xs={12} md={4}>
+          <Typography variant="h4" gutterBottom>
+            Shipper ID
+          </Typography>
+          <div style={{ width: "100%" }}>
+          <Box
+            component="form"
+            sx={{
+              '& > :not(style)': { m: 1, width: '25ch' },
+            }}
+            noValidate
+            autoComplete="off"
+            onSubmit={submitHandler}
+          >
+            <TextField id="inputId" label="Shipper Id" placeholder="between 1 to 3" variant="standard"
+            onChange={e => setcurShipperId(e.target.value)} />
+            <Button variant="contained" type="submit">Check</Button>
+          </Box>
+          <Typography variant="h6" color="error.main">
+            {errorMsg}
+          </Typography>
+          </div>
+        </Grid>
+        <Grid xs={12} md={8}>
+          <Typography variant="h4" gutterBottom align="center">
+            Shipment Table
+          </Typography>
+          <div style={{ height: 600, width: '100%' }}>
+            <DataGrid rows={curShipment} columns={columns} />
+          </div>
+        </Grid>
+      </Grid>
+    </div>
+  );
+}
+
+export default App;
