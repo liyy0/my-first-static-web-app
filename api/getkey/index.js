0968db3940a0dd8c1699f7a877cc51e67f8fa4f0
Liyy <liyy@crc-dot1x-nat-10-239-70-88.bu.edu>
2023-03-14 17:54:47 -0400

    add api testing2

--- api/getkey/index.js
+++ api/getkey/index.js
@@ -1,5 +1,5 @@
 module.exports = async function (context, req) {
-    const temp  = "hello" + process.env.REACT_APP_api_key
+    const temp  = process.env.REACT_APP_api_key
     context.res = {
         // status: 200, /* Defaults to 200 */
         body: temp
