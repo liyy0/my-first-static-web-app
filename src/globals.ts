6b78e1824426bc4c5536e2766fa12973d1fc623c
Liyy <liyy@Yuyans-MacBook-Air.local>
2023-03-13 21:58:35 -0400

    changing reading format

--- src/globals.ts
+++ src/globals.ts
@@ -7,7 +7,7 @@ export const BASE_URL = "https://function4api.azurewebsites.net/api/getshipment?
 export const GET_DEFAULT_HEADERS = () => {
   var headers = new Headers();
   headers.append('accept', 'application/json')
-  headers.append('x-functions-key',process.env.REACT_APP_api_key as string);
+  headers.append('x-functions-key',process.env["REACT_APP_api_key"] as string);
   return headers;
 };
 
