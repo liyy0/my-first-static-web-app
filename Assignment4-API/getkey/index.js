fd99d784cef71bcf60645c22670444e79f98e583
Liyy <liyy@crc-dot1x-nat-10-239-70-88.bu.edu>
2023-03-14 20:04:04 -0400

    testing download

--- Assignment4-API/getkey/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = async function (context, req) {
-    let temp  = process.env.REACT_APP_api_key
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: temp
-    };
-}
