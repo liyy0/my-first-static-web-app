cc6c6e54e10abe3cb7c53a57985fa3e16459b778
Liyy <liyy@crc-dot1x-nat-10-239-70-88.bu.edu>
2023-03-14 21:38:31 -0400

    change funcapp

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ function App() {
   const [apiKey,setapiKey] = useState<string>("default_none");
 
   const getApiKey = async () => {
-    const res = await fetch("https://assignment4-api.azurewebsites.net/api/settings", {
+    const res = await fetch("https://apigetkey.azurewebsites.net/api/pushkey", {
       method: "GET"//
     })
   
